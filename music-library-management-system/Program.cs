9792e34a32bc047da795232acf58c329ffbd5e00
Levi de Jesus <levijesus2121@gmail.com>
2026-07-01 16:59:21 -0300

    this is my first commit in here!

---
+++ music-library-management-system/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace music_library_management_system
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
